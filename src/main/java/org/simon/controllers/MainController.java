f80e15a19682a1fecea2d7cf56dd2b766a11f342
Semen Simanov <63959715+bpatapb88@users.noreply.github.com>
2020-12-24 17:01:11 +0100

    test

--- src/main/java/org/simon/controllers/MainController.java
+++ src/main/java/org/simon/controllers/MainController.java
@@ -1,19 +1,34 @@
 package org.simon.controllers;
 
+import org.simon.JsonReader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.concurrent.ExecutionException;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String home(Model model) throws ExecutionException, InterruptedException {
+    public String home(Model model) throws ExecutionException, InterruptedException, IOException {
         /*Iterable<Links> links = linksRepository.findAll();
         model.addAttribute("links", links);
         model.addAttribute("name", " World");*/
+
+        JSONObject json = JsonReader.readJsonFromUrl("https://springboot-dagrejs-default-rtdb.europe-west1.firebasedatabase.app/.json");
+        System.out.println(json.toString());
+        System.out.println("test #2  " + json.get("148570018_1100"));
+
         return "home";
     }
+
+    String firebaselink = "https://springboot-dagrejs-default-rtdb.europe-west1.firebasedatabase.app/.json";
+
+
 }
