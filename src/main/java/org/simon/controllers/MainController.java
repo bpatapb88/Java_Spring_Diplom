4e07f3e2107e4165364f3dd94177defa5c0b1806
Semen Simanov <63959715+bpatapb88@users.noreply.github.com>
2020-12-26 14:13:56 +0100

    nodes is taken from json, edges, not yet

--- src/main/java/org/simon/controllers/MainController.java
+++ src/main/java/org/simon/controllers/MainController.java
@@ -1,5 +1,6 @@
 package org.simon.controllers;
 
+import org.json.JSONArray;
 import org.simon.JsonReader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import java.io.*;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -23,8 +26,21 @@ public class MainController {
 
         JSONObject json = JsonReader.readJsonFromUrl("https://springboot-dagrejs-default-rtdb.europe-west1.firebasedatabase.app/.json");
         System.out.println(json.toString());
-        System.out.println("test #2  " + json.get("148570018_1100"));
-
+        JSONArray jsonlist = json.getJSONArray("boxes");
+        ArrayList<String> nodes = new ArrayList<>();
+        ArrayList<String[]> edges = new ArrayList<>();
+        for (int i = 0; i< jsonlist.length(); i++){
+            nodes.add(jsonlist.getJSONObject(i).getString("name"));
+            if(jsonlist.getJSONObject(i).length() == 3) {
+                for (int j = 0; j < jsonlist.getJSONObject(i).getJSONArray("links").length(); j++) {
+                    String[] temp = {jsonlist.getJSONObject(i).getString("name"), jsonlist.getJSONObject(i).getJSONArray("links").getString(j)};
+                    edges.add(temp);
+                }
+                System.out.println(jsonlist.getJSONObject(i).getJSONArray("links"));
+            }
+        }
+        model.addAttribute("nodes", nodes);
+        model.addAttribute("edges", edges);
         return "home";
     }
 
